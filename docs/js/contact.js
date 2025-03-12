e5b137467cf5f2eda3f07d94c65d993284078e62
Brian <brian@melodimedia.co.uk>
2025-03-12 10:03:55 +0000

    update info

--- docs/js/contact.js
+++ docs/js/contact.js
@@ -12,7 +12,7 @@ $(function async(){
 
     $('#submit').on('click', function(event){
         event.preventDefault();
-        let email = 'myschedulestudio@gmail.com';
+        let email = 'santana30541@gmail.com';
         let subject = $('#subject')[0].value || '';
         let emailBody = $('#message')[0].value || '';
         window.location = 'mailto:' + email + '?subject=' + subject + '&body=' + emailBody;
